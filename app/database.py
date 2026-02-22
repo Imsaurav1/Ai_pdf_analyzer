8f0bad2ef5ed7f202a67c81488722c3ddfcc58a8
Saurabh Kumar Jha <100080003+Imsaurav1@users.noreply.github.com>
2026-02-22 17:19:58 +0530

    dsafkjhlkjfas

--- app/database.py
+++ app/database.py
@@ -2,7 +2,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import MONGODB_URI
 
 client = AsyncIOMotorClient(MONGODB_URI)
-db = client["pdf_analyzer"]
+
+# Use default DB from URI
+db = client.get_default_database()
 
 users_collection = db["users"]
 analysis_collection = db["analysis"]
